c35843a9c6a624c666c71644b833f4e00abb0a0c
InBrewJ <jason@urawizard.com>
2020-10-16 21:43:46 +0100

    better box_check

--- box_check.sh
+++ box_check.sh
@@ -1,10 +1,23 @@
 NUM_TEST_PINGS=2
+TEST_DELAY=5
 DIVIDER='----------------------------------------'
-echo $DIVIDER
-echo $DIVIDER
-ping -c $NUM_TEST_PINGS speakerbox.local
-echo $DIVIDER 
-echo $DIVIDER
-ping -c $NUM_TEST_PINGS switchbox.local 
-echo $DIVIDER
-echo $DIVIDER
+
+function print_divider {
+    echo $DIVIDER
+    echo $DIVIDER
+}
+
+function check {
+    print_divider
+    ping -c $NUM_TEST_PINGS speakerbox.local
+    print_divider
+    ping -c $NUM_TEST_PINGS switchbox.local 
+    print_divider
+}
+
+while true; do 
+    check
+    sleep $TEST_DELAY
+    clear
+done
+
