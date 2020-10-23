ff5d162b4c61ba17c6c30a8ebad397f928a96e99
InBrewJ <jason@urawizard.com>
2020-10-23 20:04:53 +0100

    switched around images on the "landing page", home.urawizard update.sh now also invalidates the cloudfront cache

--- home.urawizard/update.sh
+++ home.urawizard/update.sh
@@ -1,1 +1,23 @@
-aws s3 sync site/ s3://home.urawizard.com --delete
+if [ $# -eq 0 ]; then
+    echo "Usage:"
+    echo "update.sh <deployment target>"
+    exit 1
+fi
+
+
+echo "Deploying home.urawizard to $1..."
+
+if [ $1 == "aws" ]; then
+    echo Deploying to aws
+    aws s3 sync site/ s3://home.urawizard.com --delete
+    cd infra/aws/
+    ./invalidate_cache.sh
+    echo Deployed
+else
+    echo Deployment target not supported, sorry
+    exit 1;
+fi
+
+
+
+
