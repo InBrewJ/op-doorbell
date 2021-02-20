d0d9bbf1831e245dbcf2ecd1fe3494be6a0f6bcd
InBrewJ <jason@urawizard.com>
2021-02-20 11:40:43 +0000

    tentatively modifies WizardMon/Broker to remake and rebreak the pika connection on every produce

--- WizardMon/Broker.py
+++ WizardMon/Broker.py
@@ -9,16 +9,18 @@ RABBIT_EXCHANGE = "e_WizardMon"
 
 class Broker:
     def __init__(self):
+        print("Broker module initialised...")
+
+    def produce(self, message: str):
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(RABBIT_HOST)
         )
         self.channel = self.connection.channel()
         self.channel.exchange_declare(exchange=RABBIT_EXCHANGE, exchange_type="fanout")
-
-    def produce(self, message: str):
         self.channel.basic_publish(
             exchange=RABBIT_EXCHANGE, routing_key="", body=json.dumps(message)
         )
+        self.connection.close()
 
     def __del__(self):
         self.connection.close()
